d4ea2bbe157a5bec0e6b4d35bd5623d2f974cddd
lyrickhare <80460792+lyrickhare@users.noreply.github.com>
2023-05-12 13:04:38 +0530

    other now will have more than one image

--- main/dedup/bestquality.py
+++ main/dedup/bestquality.py
@@ -7,15 +7,35 @@ def var(imgPath):
     return var
 
 
+def exactSame(img1pth,img2pth):
+    if(open(img1pth,"rb").read() == open(img2pth,"rb").read()):
+        return 1
+    else:
+        return 0
+    
+
 def bestquality(classifiedDict):
     singleDict = {"PAN":[],"DL":[],"Passport":[],"Aadhaar":[],"other":[]}
     for key in classifiedDict: 
-        varv = 0
-        pathv =""
-        for path in classifiedDict[key]:
-            varc = var(path)
-            if (varc>varv):
-                varv = varc
-                pathv = path
-        singleDict[key].append(pathv)
+        if(key!="other"):
+            varv = 0
+            pathv =""
+            for path in classifiedDict[key]:
+                varc = var(path)
+                if (varc>varv):
+                    varv = varc
+                    pathv = path
+            singleDict[key].append(pathv)
+        else:
+            l = len(classifiedDict["other"])
+            singleDict["other"] = classifiedDict["other"]
+            pop = set()
+            for i in range(l):
+                for j in range(i+1,l):
+                    if(exactSame(classifiedDict["other"][i],classifiedDict["other"][j])):
+                        pop.add(j)
+                    else:
+                        pass
+            for i in sorted(pop, reverse=True):
+                del singleDict["other"][i]
     return singleDict
