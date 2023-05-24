3a1c048fadc37fd6c35debf3b44343ca378d11ac
raghavrander <81479687+raghavrander@users.noreply.github.com>
2023-05-24 10:46:51 +0530

    Updated fastapi

--- raghav/api/app.py
+++ raghav/api/app.py
@@ -7,14 +7,17 @@ import json
 app = FastAPI()
 
 @app.post("/upload")
-async def upload(file:UploadFile = File(...)):
+async def upload(files:list[UploadFile] = File(...)):
     # contents = file.file.read()
      # with open(file.filename, 'wb') as f:
      #     f.write(contents)
-    image_bytes = await file.read() 
-    encoded_img = np.frombuffer(image_bytes,dtype=np.uint8)
-    pred = segregateNN.segregate(encoded_img)
-    return pred
+    pred_list=[]
+    for file in files:
+        image_bytes = await file.read() 
+        encoded_img = np.frombuffer(image_bytes,dtype=np.uint8)
+        pred = segregateNN.segregate(encoded_img)
+        pred_list.append(pred)
+    return(pred_list)
 
     
 
