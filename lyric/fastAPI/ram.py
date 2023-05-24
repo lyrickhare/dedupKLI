69e290f012b069bb1533def44be0b13543318579
lyrickhare <80460792+lyrickhare@users.noreply.github.com>
2023-05-24 09:24:46 +0530

    addedImageUpload

--- lyric/fastAPI/ram.py
+++ lyric/fastAPI/ram.py
@@ -1,6 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+import os
+from random import randint
+import uuid
+ 
+IMAGEDIR = "images/"
+ 
 app = FastAPI()
-
-@app.get("/")
-def ram1():
-    return{"title":"shri ganeshaya namah"}
+ 
+ 
+@app.post("/upload/")
+async def create_upload_file(file: UploadFile = File(...)):
+ 
+    file.filename = f"{uuid.uuid4()}.jpg"
+    contents = await file.read()
+ 
+    #save the file
+    with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
+        f.write(contents)
+ 
+    return {"filename": file.filename}
+ 
+ 
+@app.get("/show/")
+async def read_random_file():
+ 
+    # get random file from the image directory
+    files = os.listdir(IMAGEDIR)
+    random_index = randint(0, len(files) - 1)
+ 
+    path = f"{IMAGEDIR}{files[random_index]}"
+     
+    return FileResponse(path)
